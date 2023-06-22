7a41a9157fa0e1549f6420b550176d1c7d9caf2b
Samantha Campbell <scampbell134@gmail.com>
2023-06-22 13:05:18 -0400

    setup task grid

--- reminder-app/src/components/TaskRow.tsx
+++ reminder-app/src/components/TaskRow.tsx
@@ -1,8 +1,11 @@
 function TaskRow() {
   return (
     <>
-      <div className="grid grid-flow-row auto-rows-max">
-        <h1> Task Row</h1>
+      <div className="grid grid-rows-4">
+        <div>Task Category</div>
+        <div>Task Name</div>
+        <div>Task Frequency</div>
+        <div>Checkbox</div>
       </div>
     </>
   );
